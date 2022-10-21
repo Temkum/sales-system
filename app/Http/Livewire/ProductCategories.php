7d07eab86bfb9212301910d28d142fd21d9adf63
Kum Jude Tem <judekum14@gmail.com>
2022-10-21 16:08:20 +0100

    Search product functionality

--- app/Http/Livewire/ProductCategories.php
+++ app/Http/Livewire/ProductCategories.php
@@ -9,10 +9,20 @@ use App\Models\ProductCategory;
 class ProductCategories extends Component
 {
     use WithPagination;
+    public $search_item;
+
+    public function updatingSearch()
+    {
+    }
 
     public function render()
     {
-        $products = ProductCategory::orderBy('created_at', 'DESC')->paginate(13);
+
+        $search = '%' . $this->search_item . '%';
+
+        $products = ProductCategory::where('prod_name', 'LIKE', $search)->orWhere('price', 'LIKE', $search)->orderBy('created_at', 'DESC')->paginate(10);
+
+        $this->resetPage();
 
         return view('livewire.product-categories', ['products' => $products])->extends('base');
     }
