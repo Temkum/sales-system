812aeb9f9bc3c49496602d3e193f351f08026dd8
Kum Jude Tem <judekum14@gmail.com>
2022-12-30 20:49:16 +0100

    modified localization params

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -16,9 +16,9 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next)
     {
-        if (request('change_language')) {
-            session()->put('language', request('change_language'));
-            $language = request('change_language');
+        if (request('lang')) {
+            session()->put('language', request('lang'));
+            $language = request('lang');
         } elseif (session('language')) {
             $language = session('lanaguage');
         } elseif (config('app.locale')) {
