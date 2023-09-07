23a3b86f0373c3037a90d723b8dcbd9441f0697d
Kum Jude Tem <judekum14@gmail.com>
2023-09-07 23:20:57 +0100

    Fix menu bug

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8" />
   <meta name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-  <title>{{ __('Dashboard | Admin') }}</title>
+  <title>{{ __('Pacho Design') }}</title>
   <meta name="description" content="" />
   <link rel="icon" type="image/x-icon" href="#" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -14,31 +14,18 @@
   <link
     href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
     rel="stylesheet" />
-
-  <!-- Icons. Uncomment required icon fonts -->
-  <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css') }}" />
-  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
-  <!-- Core CSS -->
-  <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}"
-    class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
-
-  <!-- Vendors CSS -->
-  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}" />
-  <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}" />
-  <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
-
-  <script src="{{ asset('assets/js/config.js') }}"></script>
-  <script src="{{ asset('assets/js/custom.js') }}"></script>
-
+  <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
+  <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="../assets/css/demo.css" />
+  <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <link rel="stylesheet" href="../assets/vendor/libs/apex-charts/apex-charts.css" />
+  <script src="../assets/vendor/js/helpers.js"></script>
+  <script src="../assets/js/config.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
-
   <link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/5.0.7/sweetalert2.min.css" rel="stylesheet">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+  <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 
   @livewireStyles
 </head>
