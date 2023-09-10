fa3df5f2c4019a32269e4ee5cda47b72972f8f20
Kum Jude Tem <judekum14@gmail.com>
2023-09-11 00:55:39 +0100

    Upgrade to vite from laravel mix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -20,9 +20,9 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 // import Echo from "laravel-echo";
 
-// window.Pusher = require('pusher-js');
-// import Pusher from "pusher-js";
-// window.Pusher = Pusher;
+window.Pusher = require("pusher-js");
+import Pusher from "pusher-js";
+window.Pusher = Pusher;
 
 // window.Echo = new Echo({
 //     broadcaster: "pusher",
